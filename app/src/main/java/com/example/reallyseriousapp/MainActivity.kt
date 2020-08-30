f915e48fa8b8ac15477fc517154b480846cc2854
Mohsin Abbasi <42749805+mabbasi5-ford@users.noreply.github.com>
2020-08-30 02:01:57 -0400

    Added Dagger 2.0 for a cleaner Code Base
    Added libaries necessary for Dagger 2.0 implementaiton.
    Added Sample Module for example purposes

--- app/src/main/java/com/example/reallyseriousapp/MainActivity.kt
+++ app/src/main/java/com/example/reallyseriousapp/MainActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.reallyseriousapp.databinding.ActivityMainBinding
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    var viewModel : UserViewModel = UserViewModel()
+    @Inject
+    lateinit var userInfoViewModel : UserInfoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
         setContentView(R.layout.activity_main)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = viewModel
+        binding.viewModel = userInfoViewModel
     }
 }
---
+++ app/src/main/java/com/example/reallyseriousapp/MyApplication.kt
@@ -0,0 +1,24 @@
+package com.example.reallyseriousapp
+
+import android.app.Activity
+import android.app.Application
+import com.example.reallyseriousapp.dagger.DaggerMyApplicationComponent
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
+
+class MyApplication : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    @Override
+    override fun onCreate() {
+        super.onCreate()
+        DaggerMyApplicationComponent.create().inject(this)
+    }
+
+    override fun activityInjector(): DispatchingAndroidInjector<Activity> {
+        return dispatchingAndroidInjector
+    }
+}
